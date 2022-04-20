c2a78493ca0bbc267ca27ad4750515fb8b6282ee
m-skowronski <mskowronski547@gmail.com>
2022-04-20 23:09:08 +0200

    3.2 zmiana konwencji nazewniczej metod

--- src/main/java/edu/kis/vh/nursery/factory/RhymersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/RhymersFactory.java
@@ -3,8 +3,8 @@ package edu.kis.vh.nursery.factory;
 import edu.kis.vh.nursery.DefaultCountingOutRhymer;
 
 public interface RhymersFactory {
-	public DefaultCountingOutRhymer GetStandardRhymer();
-	public DefaultCountingOutRhymer GetFalseRhymer();
-	public DefaultCountingOutRhymer GetFIFORhymer();
-	public DefaultCountingOutRhymer GetHanoiRhymer();
+	public DefaultCountingOutRhymer getStandardRhymer();
+	public DefaultCountingOutRhymer getFalseRhymer();
+	public DefaultCountingOutRhymer getFIFORhymer();
+	public DefaultCountingOutRhymer getHanoiRhymer();
 }
