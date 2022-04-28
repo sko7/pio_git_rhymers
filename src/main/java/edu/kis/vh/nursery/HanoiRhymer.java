e31b2a75a82c21e9a88983f5224ba701b436916e
m-skowronski <mskowronski547@gmail.com>
2022-04-28 02:06:59 +0200

    8.1 Dodanie brakujacego private

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,7 +2,7 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
-	public static final int REJECTED_INITIALIZER = 0;
+	private static final int REJECTED_INITIALIZER = 0;
 	private int totalRejected = REJECTED_INITIALIZER;
 
 	public int reportRejected() {
